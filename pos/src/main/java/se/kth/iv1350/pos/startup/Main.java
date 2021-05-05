d97a3ffacc492abd85635666422fed4dc63069d9
Alexander Lundqvist <alexander.lundqvist@outlook.com>
2021-05-06 00:35:58 +0200

    Initial commit

---
+++ pos/src/main/java/se/kth/iv1350/pos/startup/Main.java
@@ -0,0 +1,18 @@
+package se.kth.iv1350.pos.startup;
+
+import se.kth.iv1350.pos.controller.Controller;
+import se.kth.iv1350.pos.view.View;
+
+/**
+ *
+ * @author Zander
+ */
+public class Main {
+    /**
+     * Main method used to start application
+     * @param args The application does not take any command line paramters
+     */
+    public static void main(String[] args){
+        
+    }
+}
